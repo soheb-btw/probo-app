0bcc9d33abea85ec7574dbc27c80414f2a27f49e
Soheb Attar <sohebattar1999@gmail.com>
2024-12-06 16:16:25 +0530

    Changed Home page design

--- probo-frontend/src/pages/Home.tsx
+++ probo-frontend/src/pages/Home.tsx
@@ -1,69 +1,274 @@
-// import React from 'react';
-import { TrendingUp, Users, Trophy } from 'lucide-react';
-// import { CategoryList } from '../components/Markets/CategoryList';
+import { useEffect, useState } from "react";
+import headerImage from "../../public/assets/lendingheader.avif";
+// import { Download } from "./download";
+import { useNavigate } from "react-router-dom";
+import video from "../../public/assets/video/info-video.mp4";
+import startqoutes from "../../public/assets/Startquotes.avif";
+import endqoutes from "../../public/assets/Endquotes.avif";
+import nazar from "../../public/assets/nazar.avif";
+import khabar from "../../public/assets/khabar.avif";
+import jigar from "../../public/assets/jigar.avif";
+import sabar from "../../public/assets/sabar.avif";
+import person1 from "../../public/assets/person1.png";
+import person2 from "../../public/assets/person2.png";
+import { Download } from "@/components/download";
+import { ArrowRight } from "lucide-react";
+
+const data = {
+  samachar: {
+    heading: "Be in the know",
+    text: "From Sports to Entertainment, Economy, Finance and more. Keep an eye on events in your field of interest.",
+  },
+  vichaar: {
+    heading: "Use what you know",
+    text: "Build your knowledge and form your opinions and views about upcoming events in the world.",
+  },
+  vyapaar: {
+    heading: "Trade and grow",
+    text: "Invest in your opinions about future events and use your knowledge to trade & benefit.",
+  },
+};
 
 export const Home = () => {
+  const [validAge, setValidAge] = useState(true);
+  const [isDownload, setIsDownload] = useState(false);
+  const [sectionTab, setSectionTab] = useState<
+    "samachar" | "vichaar" | "vyapaar"
+  >("samachar");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const tabs: ("samachar" | "vichaar" | "vyapaar")[] = [
+      "samachar",
+      "vichaar",
+      "vyapaar",
+    ];
+    let currentIndex = 0;
+
+    const timeId = setInterval(() => {
+      currentIndex = (currentIndex + 1) % tabs.length;
+      setSectionTab(tabs[currentIndex]);
+    }, 5000);
+
+    return () => clearInterval(timeId);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Hero Section */}
-      <div className="bg-indigo-600 text-white py-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">
-              Trade Your Predictions, Shape the Future
+    <>
+      <div className="">
+        <section
+          id="header"
+          className="bg-[url('../assets/header-bg.svg')] w-full min-h-[650px] flex justify-between"
+        >
+          <div>
+
+          </div>
+          <div className="flex flex-col pl-20 p-10 justify-center" id="left">
+            <h1 className="text-[80px] font-normal work-sans">
+              Invest in your
             </h1>
-            <p className="text-xl text-indigo-200 mb-8">
-              Join India's largest prediction marketplace and trade on future events
-            </p>
-            <button className="bg-white text-indigo-600 px-8 py-3 rounded-md font-medium hover:bg-indigo-50">
-              Start Trading Now
-            </button>
+            <h2 className="text-[56px] work-sans font-normal -mt-2">
+              point of view
+            </h2>
+            <h5 className="text-[22px] font-normal work-sans text-[#545454] mt-5">
+              Sports, Entertainment, Economy or Finance.
+            </h5>
+            <div className="mt-10 flex gap-5">
+              <button
+                onClick={() => setIsDownload(true)}
+                disabled={!validAge}
+                className={`border rounded px-10 py-2 ${validAge
+                  ? "text-black bg-white"
+                  : "text-white bg-[#ABABAB] cursor-not-allowed"
+                  } `}
+              >
+                Download App
+              </button>
+              <button
+                onClick={() => navigate("/events")}
+                disabled={!validAge}
+                className={`"border rounded px-10 py-2 ${validAge
+                  ? "text-white bg-black"
+                  : "text-white bg-[#ABABAB] cursor-not-allowed"
+                  }`}
+              >
+                Trade Online
+              </button>
+            </div>
+            <span className="flex items-center ">
+              <input
+                checked={validAge}
+                onChange={() => setValidAge(!validAge)}
+                className="w-4 h-4 accent-black mt-4"
+                type="checkbox"
+                name="age"
+              />
+              <span className="text-xs font-medium mt-4 ml-2 text-[#757575]">
+                For 18 years and above only
+              </span>
+            </span>
           </div>
-        </div>
-      </div>
+          <div id="right" className="pr-20">
+            <img src={headerImage} alt="header" />
+          </div>
+        </section>
+        <section
+          id="info-section"
+          className="bg-[#262626] py-14 px-36 w-full min-h-[630px] flex justify-between"
+        >
+          <div className=" w-[55%] py-44">
+            <div
+              onClick={(e: any) => {
+                if (e.target.nodeName === "BUTTON") {
+                  let value = e.target.getAttribute("value");
+                  console.log('valuealalkfd', value);
+                  setSectionTab(value);
+                }
+              }}
+              className="flex gap-5"
+            >
+              <button
+                value="samachar"
+                className={`${sectionTab === "samachar"
+                  ? "text-[#FFFFFF] font-semibold"
+                  : "text-[#757575] font-normal"
+                  }  text-5xl work-sans`}
+              >
+                Samachar
+              </button>
+              <button
+                value="vichaar"
+                className={`${sectionTab === "vichaar"
+                  ? "text-[#FFFFFF] font-semibold"
+                  : "text-[#757575] font-normal"
+                  }  text-5xl work-sans`}
+              >
+                Vichaar
+              </button>
+              <button
+                value="vyapaar"
+                className={`${sectionTab === "vyapaar"
+                  ? "text-[#FFFFFF] font-semibold"
+                  : "text-[#757575] font-normal"
+                  }  text-5xl work-sans`}
+              >
+                Vyapaar
+              </button>
+            </div>
+            <div className="mt-10 flex flex-col gap-y-5">
+              <h1 className="text-white work-sans text-4xl font-semibold">
+                {data[sectionTab]?.heading}
+              </h1>
+              <p className="text-white work-sans text-4xl font-normal">
+                {data[sectionTab]?.text}
+              </p>
+            </div>
+          </div>
+          <div className="border-[12px] mr-14 border-[#3f3f3f] rounded-[38px] w-80 h-fit">
+            <video
+              className="rounded-[30px] h-fit"
+              id="info-video"
+              autoPlay
+              playsInline
+              muted
+              loop
+              preload="auto"
+            >
+              <source src={video} type="video/mp4" />
+            </video>
+          </div>
+        </section>
+        <section className="bg-[#F5F5F5] w-full min-h-[630px] px-24 py-20 pb-20">
+          <div className="text-[#262626] ml-48 items-center work-sans text-[64px] font-semibold flex">
+            <img
+              width={70}
+              height={70}
+              className="object-contain h-fit mr-4"
+              src={startqoutes}
+              alt="qoute"
+            />
+            <span className="w-full h-fit">News that creates trading </span>
+          </div>
+          <div className="text-[#262626]  ml-[40%] items-center work-sans w-fit gap-4 text-[64px] font-semibold flex">
+            <span className="w-full h-fit">opportunity, everyday</span>
+            <img
+              width={70}
+              height={70}
+              className="object-contain h-fit rotate-180"
+              src={endqoutes}
+              alt="qoute"
+            />
+          </div>
+          <div onClick={() => navigate('/events')} className="flex gap-10 mt-20">
+            <div className="w-1/4 relative flex flex-col items-center">
+              <div className="relative">
+                <img src={nazar} alt="nazar" />
+                <div className="absolute inset-0 bg-purple-200 rounded-full -z-10"></div>
+              </div>
+              <div className="absolute bottom-12 transform translate-y-1/2 bg-white p-4 w-11/12 text-center shadow-lg">
+                <h1 className="text-black text-xl font-semibold mb-2">Nazar</h1>
+                <p className="text-gray-700">
+                  Keep an eye on the happenings around you. Be it Politics,
+                  Sports, Entertainment and more.
+                </p>
+              </div>
+            </div>
 
-      {/* Features Section */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <h2 className="text-3xl font-bold text-center mb-12">Why OpinionTrade?</h2>
-        
-        <div className="grid md:grid-cols-3 gap-8">
-          <div className="text-center">
-            <div className="bg-indigo-100 p-4 rounded-full inline-block mb-4">
-              <TrendingUp className="h-8 w-8 text-indigo-600" />
+            <div className="w-1/4 relative flex flex-col items-center">
+              <div className="relative">
+                <img src={khabar} alt="khabar" />
+                <div className="absolute inset-0 bg-purple-200 rounded-full -z-10"></div>
+              </div>
+              <div className="absolute bottom-12 transform translate-y-1/2 bg-white p-4 w-11/12 text-center shadow-lg">
+                <h1 className="text-black text-xl font-semibold mb-2">
+                  Khabar
+                </h1>
+                <p className="text-gray-700">
+                  Understand the news without the noise. Get to the crux of
+                  every matter and develop an opinion.
+                </p>
+              </div>
             </div>
-            <h3 className="text-xl font-semibold mb-2">Real-time Trading</h3>
-            <p className="text-gray-600">
-              Trade predictions on various topics with dynamic pricing
-            </p>
-          </div>
-          
-          <div className="text-center">
-            <div className="bg-indigo-100 p-4 rounded-full inline-block mb-4">
-              <Users className="h-8 w-8 text-indigo-600" />
+            <div className="w-1/4 relative flex flex-col items-center">
+              <div className="relative">
+                <img src={jigar} alt="jigar" />
+                <div className="absolute inset-0 bg-purple-200 rounded-full -z-10"></div>
+              </div>
+              <div className="absolute bottom-12 transform translate-y-1/2 bg-white p-4 w-11/12 text-center shadow-lg">
+                <h1 className="text-black text-xl font-semibold mb-2">Jigar</h1>
+                <p className="text-gray-700">
+                  Have the courage to stand by your opinions about upcoming
+                  world events by investing in them.
+                </p>
+              </div>
             </div>
-            <h3 className="text-xl font-semibold mb-2">Community Wisdom</h3>
-            <p className="text-gray-600">
-              Leverage collective intelligence for better predictions
-            </p>
-          </div>
-          
-          <div className="text-center">
-            <div className="bg-indigo-100 p-4 rounded-full inline-block mb-4">
-              <Trophy className="h-8 w-8 text-indigo-600" />
+            <div className="w-1/4 relative flex flex-col items-center">
+              <div className="relative">
+                <img src={sabar} alt="sabar" />
+                <div className="absolute inset-0 bg-purple-200 rounded-full -z-10"></div>
+              </div>
+              <div className="absolute bottom-12 transform translate-y-1/2 bg-white p-4 w-11/12 text-center shadow-lg">
+                <h1 className="text-black text-xl font-semibold mb-2">Sabar</h1>
+                <p className="text-gray-700">
+                  Have the patience to negotiate market ups and downs, and take
+                  a decision as events unfold.
+                </p>
+              </div>
             </div>
-            <h3 className="text-xl font-semibold mb-2">Probo Team 11</h3>
-            <p className="text-gray-600">
-              Create your fantasy cricket team and compete for rewards
-            </p>
           </div>
-        </div>
+        </section>
+        <section className="bg-black py-14 relative px-36 flex justify-center min-h-[630px]">
+          <img src={person1} alt="image" className="w-[20rem] absolute left-[5%] bottom-0" />
+          <div className="flex flex-col items-center gap-20">
+            <h1 className="text-white work sans text-8xl text-center">
+              What will be the return on your opinions?
+            </h1>
+            <button className="bg-dark text-white px-14 py-4 rounded-[58px] text-2xl border"><span className="text-xl flex gap-2 items-center">Download App <ArrowRight/></span></button>
+          </div>
+          <img src={person2} alt="image" className="w-[20rem] absolute right-[5%] bottom-0" />
+        </section>
       </div>
-
-      {/* Categories Section */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 bg-white">
-        <h2 className="text-3xl font-bold text-center mb-12">Explore Categories</h2>
-        {/* <CategoryList /> */}
-      </div>
-    </div>
+      {isDownload && <Download setIsDownload={setIsDownload} />}
+    </>
   );
 };
--- probo-frontend/src/pages/Trade.tsx
+++ probo-frontend/src/pages/Trade.tsx
@@ -1,99 +1,20 @@
 import PlaceOrder from '@/components/placeOrder';
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import '../styles/gradients.css';
+import Orderbook from '@/components/orderbook';
+import EventChart from '@/components/EventChart';
 
-interface OrderBookEntry {
-  price: number;
-  qtyYes: number;
-  qtyNo: number;
-}
-
-interface TradeProps {}
+interface TradeProps { }
 
 const Trade: React.FC<TradeProps> = () => {
- 
-  const orderBookData: OrderBookEntry[] = [
-    { price: 4.5, qtyYes: 6, qtyNo: 9 },
-    { price: 5.5, qtyYes: 5, qtyNo: 3 },
-    { price: 6.0, qtyYes: 50, qtyNo: 4 },
-    { price: 6.5, qtyYes: 49, qtyNo: 2 },
-    { price: 7.0, qtyYes: 130, qtyNo: 1 },
-  ];
-
-  // Calculate maximum quantities for scaling
-  const maxYesQty = Math.max(...orderBookData.map(entry => entry.qtyYes));
-  const maxNoQty = Math.max(...orderBookData.map(entry => entry.qtyNo));
-
   return (
-    <div className="flex bg-gray-100 p-20 text-sm">
-      <div className="w-1/2 p-8 bg-white shadow-lg rounded-lg">
-        <div className="space-y-2">
-          <div className="flex gap-4 mb-6">
-            <h2 className="text-lg font-semibold border-b-2 border-black">Order Book</h2>
-          </div>
-
-          <div className="flex gap-4">
-            {/* YES Table */}
-            <div className="flex-1">
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th className="text-left font-semibold pb-2">PRICE</th>
-                    <th className="text-right font-semibold pb-2">
-                      QTY AT <span className="text-blue-500">YES</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orderBookData.map((entry, index) => (
-                    <tr 
-                      key={`yes-${index}`} 
-                      className="hover:bg-gray-50 bg-gradient-yes border-b-2 border-gray-200"
-                      style={{ 
-                        '--gradient-width': `${(entry.qtyYes / maxYesQty) * 100}%`
-                      } as React.CSSProperties}
-                    >
-                      <td className="py-1 relative z-10">{entry.price.toFixed(1)}</td>
-                      <td className="text-right py-1 relative z-10">{entry.qtyYes}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-
-            {/* NO Table */}
-            <div className="flex-1">
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th className="text-left font-semibold pb-2">PRICE</th>
-                    <th className="text-right font-semibold pb-2">
-                      QTY AT <span className="text-red-500">NO</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orderBookData.map((entry, index) => (
-                    <tr 
-                      key={`no-${index}`} 
-                      className="hover:bg-gray-50 bg-gradient-no relative"
-                      style={{ 
-                        '--gradient-width': `${(entry.qtyNo / maxNoQty) * 100}%`
-                      } as React.CSSProperties}
-                    >
-                      <td className="py-1 relative z-10">{(entry.price + 0.5).toFixed(1)}</td>
-                      <td className="text-right py-1 relative z-10">{entry.qtyNo}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+    <div className="flex px-20 gap-10">
+      <div className='flex flex-col gap-5 flex-grow'>
+        <Orderbook />
+        <EventChart />
       </div>
-
-     <PlaceOrder/>
+      <div className="w-[400px]">
+        <PlaceOrder />
+      </div>
     </div>
   );
 };
